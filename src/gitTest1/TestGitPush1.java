17252817e635508c98dcb076f0f82628762031a2
KaseyUchuya <Kasey.Uchuya7@gmail.com>
2020-06-11 21:51:02 -0400

    second code push

--- src/gitTest1/TestGitPush1.java
+++ src/gitTest1/TestGitPush1.java
@@ -2,7 +2,7 @@ package gitTest1;
 
 public class TestGitPush1 {
 	public static void main(String[] args) {
-		System.out.println("Syntax Technologies");
+		System.out.println("Hello world");
 	}
 	
 }
